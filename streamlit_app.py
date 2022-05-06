5674f2e12ddd44966bce87d3431e86c568617582
GAURAV CHAUHAN <104934389+Gaurav07chauhan@users.noreply.github.com>
2022-05-06 11:22:21 +0530

    Test Purpose

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit
 
 
 streamlit.title('My Parent New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+
+streamit.text('Hard-Boiled Free-Range Egg')
